f25f3a9c901e09a4dce0919af0bd0636ef012b47
Nouma <39864318+Noumaa@users.noreply.github.com>
2023-09-01 02:59:34 +0200

    Add download

--- mods/urls.py
+++ mods/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.mods_list, name='list'),
     path('publish', views.mods_create, name='create'),
     path('<str:username>/<slug:mod_slug>', views.mods_detail, name='detail'),
+    path('<str:username>/<slug:mod_slug>/<slug:version_slug>', views.version_download, name='version-download'),
 ]
--- mods/views.py
+++ mods/views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from mods.forms import ModSubmitForm
 from mods.models import Mod, Version
@@ -40,5 +40,18 @@ def mods_detail(request, username, mod_slug):
     mod = get_object_or_404(Mod, slug=mod_slug, user=user)
     return render(request, 'mods/detail.html', {
         'mod': mod,
-        'versions': mod.versions.all()
+        'versions': mod.versions.all(),
+        'version': 'none'
+    })
+
+
+def version_download(request, username, mod_slug, version_slug):
+    user = get_object_or_404(User, username=username)
+    mod = get_object_or_404(Mod, slug=mod_slug, user=user)
+    version = get_object_or_404(Version, mod=mod, slug=version_slug)
+
+    return render(request, 'mods/detail.html', {
+        'mod': mod,
+        'versions': mod.versions.all(),
+        'version': version
     })
