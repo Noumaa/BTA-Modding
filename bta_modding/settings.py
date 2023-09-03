8cadb3e32d7a4a3a263bb0ae2eba53a7fbf340ae
Nouma <39864318+Noumaa@users.noreply.github.com>
2023-09-03 04:44:48 +0200

    Update settings.py

--- bta_modding/settings.py
+++ bta_modding/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1=zsof=)+0fq9)f_%n0_7vo3=asy+hih^yfl-1jm_=4#pnagpl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
