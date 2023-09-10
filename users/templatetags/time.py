71fb983479f50f8eb14786ec817979cc305f6de9
Nouma <39864318+Noumaa@users.noreply.github.com>
2023-09-10 08:43:36 +0200

    Fix `days_until` filter

--- users/templatetags/time.py
+++ users/templatetags/time.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
 from django import template
 
@@ -7,5 +7,27 @@ register = template.Library()
 
 @register.filter
 def days_until(date):
-    delta = datetime.date(date) - datetime.now().date()
-    return str(delta.days)[1:] if str(delta.days)[1:] is not '' else '0'
+    date = date
+    now = datetime.now(timezone.utc)
+
+    print(date)
+    print(now)
+
+    difference = now - date
+
+    seconds = difference.total_seconds()
+
+    if seconds < 60:
+        return f'{int(seconds)} seconds'
+    elif seconds < 3600:
+        minutes = seconds / 60
+        return f'{int(minutes)} minutes'
+    elif seconds < 86400:
+        hours = seconds / 3600
+        return f'{int(hours)} hours'
+    elif seconds < 2629800:
+        days = seconds / 86400
+        return f'{int(days)} days'
+    else:
+        months = seconds / 2629800
+        return f'{int(months)} months'
