cf7f6ff6dfe0fc3becb51db6763aa1f0c77eab8a
Nouma <39864318+Noumaa@users.noreply.github.com>
2023-10-08 17:50:36 +0200

    Update views.py

--- api/views.py
+++ api/views.py
@@ -14,8 +14,8 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 
-@authentication_classes([TokenAuthentication])
-@permission_classes([IsAuthenticated])
+# @authentication_classes([TokenAuthentication])
+# @permission_classes([IsAuthenticated])
 class ModViewSet(viewsets.ModelViewSet):
     queryset = Mod.objects.all()
     serializer_class = ModSerializer
