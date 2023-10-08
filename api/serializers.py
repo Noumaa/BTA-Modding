16fdb946f816850b5061978ed7c6fe7817424c96
Nouma <39864318+Noumaa@users.noreply.github.com>
2023-10-08 18:17:24 +0200

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -4,19 +4,19 @@ from mods.models import Mod, Version
 from users.models import User
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'is_staff']
 
 
-class ModSerializer(serializers.HyperlinkedModelSerializer):
+class ModSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mod
         fields = ['pk', 'slug', 'label', 'short_description']
 
 
-class VersionSerializer(serializers.HyperlinkedModelSerializer):
+class VersionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Version
-        fields = ['slug', 'label', 'changelog', 'file']
+        fields = ['pk', 'mod', 'slug', 'label', 'changelog', 'file']
